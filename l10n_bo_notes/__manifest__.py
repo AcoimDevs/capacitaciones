613c0e5d6980a1fc518a19018aa24951d0b0183b
LuisFlores2170 <acoimdesarrollos@gmail.com>
2024-07-27 09:56:06 -0400

    [ADMIN] add new model l10n_bo_notes

---
+++ l10n_bo_notes/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'apuntes internos',
+    'version': '1.0', 
+    'author':'Acoim Ltda',
+    'summary': 'Apuntes de usuarios',
+    'depends':[
+        'base',
+        'base_setup',
+        'purchase'
+    ],
+    'category': 'Notes',
+    'data': [
+        'security/ir.model.access.csv',
+        'security//ir_rule.xml',
+        'views/l10n_bo_notes.xml',
+        'views/menuitem.xml',
+        'reports/l10n_bo_notes_report.xml'
+    ],
+    'installable' : True,
+    'application': True,
+    'auto_install': False,
+    'license': 'OPL-1',
+    'webside': 'https://www.acoim.com/',
+    'maintainer': '',
+    'contributors':['luis', 'manuel','ale']
+
+}
---
+++ l10n_bo_notes/models/l10n_bo_notes.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, models, fields
+class L10nBoNotes(models.Model):
+    _name = 'l10n.bo.notes' 
+    _description = 'Notas internas'
+
+    #no es obligaroeio pero si es necesario (-> display name)
+    name = fields.Char(
+        string='nombre',
+        required=True,
+        default='Nota interna'
+         
+    )
+    
+    check = fields.Boolean(
+        string='validar',
+    )
+    
+    company_id = fields.Many2one(
+        string='Compañia', 
+        comodel_name='res.company', 
+        required=True, 
+        default=lambda self: self.env.company #lo veremos despues
+    )
+    
+    
+    purchase_id = fields.Many2one(
+        string='Compra',
+        comodel_name='purchase.order',
+        ondelete='restrict',
+    )
+    
+    
+    #char
+    #boolean
+    #integer
+    #fload
